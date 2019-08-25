de2822be1ffe54238010fafe2b3086be4fd2b1b8
KevinVandy <kevinvandy656@gmail.com>
2019-08-25 12:17:10 -0500

    server stuff

--- old/server.js
+++ old/server.js
@@ -6,10 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
-// Init Middleware
-app.use(express.json({ extended: false }));
+app.get('/', (request, response) => response.send({ msg: 'Welcome' }));
 
-// Define Routes
+//Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
